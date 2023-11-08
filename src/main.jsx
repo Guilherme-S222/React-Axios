ee9e9ed132774af10622bcdf34acfda0f3000b13
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2023-11-08 00:46:02 -0300

    atualização 08-11

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 //páginas
 import Home from './routes/Home.jsx';
 import NewPost from './routes/NewPost.jsx';
+import Post from './routes/Post.jsx';
 
 import './index.css'
 
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
         path: "/new",
         element: <NewPost />
       },
+      {
+        path: "/posts/:id",
+        element: <Post />
+      }
     ],
   },
 ]);
---
+++ src/routes/Post.jsx
@@ -0,0 +1,37 @@
+import blogFetch from "../axios/config";
+
+import { useState, useEffect } from "react";
+
+import { useParams } from "react-router-dom";
+
+import "./Post.css";
+
+const Post = () => {
+
+    const {id} = useParams();
+
+    const [post, setPost] = useState({});
+
+    const getPost = async() => {
+        try{
+            console.log(id);
+            const response = await blogFetch.get('/posts/${id}');
+
+        } catch (error){
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getPost()
+    }, []);
+
+  return (
+
+    <div className="post-container">
+        {!post.title ? (<p>Carregando...</p>) : (<div className="post"><h2>{post.title}</h2><p>{post.body}</p></div>)}
+    </div>
+  );
+}
+
+export default Post

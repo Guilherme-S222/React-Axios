6ffb43db67afc96605585a4853801435268f6766
Guilherme-S222 <124743404+Guilherme-S222@users.noreply.github.com>
2023-11-09 17:44:55 -0300

    Comments NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom" //O Link é usado para criar links de navegação em um aplicativo React que se integram ao sistema de roteamento para navegar entre diferentes páginas ou rotas.
 
 import "./NavBar.css";
 
@@ -28,3 +28,5 @@ const NavBar = () => {
 }
 
 export default NavBar
+
+//No geral, o componente NavBar cria uma barra de navegação com links que permitem a navegação dentro do aplicativo. Os links são gerenciados pelo sistema de roteamento do react-router-dom, permitindo que o usuário acesse diferentes partes do aplicativo com base nas URLs fornecidas. Os estilos específicos para a barra de navegação são definidos no arquivo "NavBar.css".

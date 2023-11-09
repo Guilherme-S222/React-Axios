301c5c25237da8b8a1d450f112ed0989708c5fc1
Guilherme-S222 <124743404+Guilherme-S222@users.noreply.github.com>
2023-11-09 17:40:34 -0300

    Comments App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import NavBar from './components/NavBar'
 
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom' //O Outlet é usado para renderizar os componentes correspondentes com base nas rotas definidas no roteador.
 
 import './App.css'
 
-function App() {
+function App() { // Este é o componente principal chamado App. Ele é uma função que retorna JSX, definindo a estrutura do aplicativo.
 
   return (
     <div className='App'>
@@ -16,4 +16,7 @@ function App() {
   )
 }
 
+//<Outlet /> é onde o roteamento entra em ação. O Outlet é um ponto de saída que corresponderá e renderizará os componentes com base nas rotas definidas anteriormente no objeto router. Os componentes específicos a serem renderizados serão determinados pela URL atual do aplicativo.
+
 export default App
+//O componente App é exportado no final do arquivo e é o ponto de entrada do seu aplicativo, que envolve outros componentes, como a barra de navegação, e usa o Outlet para exibir o conteúdo apropriado com base nas rotas.
